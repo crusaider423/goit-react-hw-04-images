6e846d6b4fd294ea1f00c0259fb81b467668796f
Олександр Канюка <137859612+crusaider423@users.noreply.github.com>
2024-01-24 16:32:24 +0200

    update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,31 +1,29 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { ImCross } from 'react-icons/im';
 import css from './Modal.module.css';
 
 const modalRoot = document.getElementById('modal-root');
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.closeModal);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.closeModal);
-  }
-  closeModal = ({ target, currentTarget, code }) =>
-    (target === currentTarget || code === 'Escape') && this.props.close();
 
-  render() {
-    const { children, close } = this.props;
-    return createPortal(
-      <div className={css.overlay} onClick={this.closeModal}>
-        <div className={css.modal}>
-          <span className={css.close} onClick={close}>
-            <ImCross />
-          </span>
-          {children}
-        </div>
-      </div>,
-      modalRoot
-    );
+export const Modal = ({ close, children }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', closeModal);
+    return () => window.removeEventListener('keydown', closeModal);
+  });
+
+  function closeModal({ target, currentTarget, code }) {
+    return (target === currentTarget || code === 'Escape') && close();
   }
-}
+
+  return createPortal(
+    <div className={css.overlay} onClick={closeModal}>
+      <div className={css.modal}>
+        <span className={css.close} onClick={close}>
+          <ImCross />
+        </span>
+        {children}
+      </div>
+    </div>,
+    modalRoot
+  );
+};
